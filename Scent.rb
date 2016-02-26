c6181296e75fd2c77654d4df736d2327122c55a4
Hassan Minhas <Hassan@Hassans-MacBook-Pro.local>
2016-02-26 19:42:08 +0000

    initialise scent class

--- Scent.rb
+++ Scent.rb
@@ -1,3 +1,11 @@
 class Scent
+  attr_accessor :scent_x
+  attr_accessor :scent_y 
+  attr_accessor :scent_direction
+  def initialize(x, y, direction)
+  	@scent_x = x
+  	@scent_y = y
+  	@scent_direction = direction
+  end
 
 end
