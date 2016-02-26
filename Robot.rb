99b1b4d059ab9ddcc775396c280222c8d49bfd30
Hassan Minhas <Hassan@Hassans-MacBook-Pro.local>
2016-02-26 19:30:24 +0000

    added way to access robot's variables

--- Robot.rb
+++ Robot.rb
@@ -1,48 +1,51 @@
 class Robot
-	def initialize(x, y, direction, commands)
-	  @x = x
-	  @y = y
-	  @direction = direction
-	  @commands = commands.split("")
-	end
+  attr_accessor :x
+  attr_accessor :y
+  attr_accessor :direction
+  attr_accessor :commands
+  def initialize(x, y, direction, commands)
+	@x = x
+	@y = y
+	@direction = direction
+	@commands = commands.split("")
+   end
 
-	def left 
-	  case @direction
-	  when 'N'
-		@direction = 'W'
-	  when 'E'
-		@direction = 'N'
-	  when 'S'
-		@direction = 'E'
-	  when 'W'
-		@direction = 'S'
-	  end
-	end
+  def left 
+  	case @direction
+  	when 'N'
+	  @direction = 'W'
+  	when 'E'
+	  @direction = 'N'
+  	when 'S'
+	  @direction = 'E'
+  	when 'W'
+	  @direction = 'S'
+  	end
+  end
 
-	def right 
-	  case @direction
-	  when 'N'
-		@direction = 'E'
-	  when 'E'
-		@direction = 'S'
-	  when 'S'
-		@direction = 'W'
-	  when 'W'
-		@direction = 'N'
-	  end
+  def right 
+	case @direction
+	when 'N'
+	  @direction = 'E'
+	when 'E'
+	  @direction = 'S'
+	when 'S'
+	  @direction = 'W'
+	when 'W'
+ 	  @direction = 'N'
 	end
+  end
 
-	def foward 
-	  case @direction
-	  when 'N'
-		@y += 1
-	  when 'E'
-		@x += 1
-	  when 'S'
-		@y -= 1
-	  when 'W'
-		@x -= 1
-	  end
-	end
-
+  def foward 
+  	case @direction
+  	when 'N'
+	  @y += 1
+  	when 'E'
+	  @x += 1
+  	when 'S'
+	  @y -= 1
+  	when 'W'
+	  @x -= 1
+    end
+  end
 end
