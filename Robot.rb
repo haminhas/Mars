8c37522a0ce75ae92eeccff0b4815ba4f3a44d46
Hassan Minhas <Hassan@Hassans-MacBook-Pro.local>
2016-02-26 19:22:18 +0000

    addded methods to move the robot

--- Robot.rb
+++ Robot.rb
@@ -6,4 +6,43 @@ class Robot
 	  @commands = commands.split("")
 	end
 
+	def left 
+	  case @direction
+	  when 'N'
+		@direction = 'W'
+	  when 'E'
+		@direction = 'N'
+	  when 'S'
+		@direction = 'E'
+	  when 'W'
+		@direction = 'S'
+	  end
+	end
+
+	def right 
+	  case @direction
+	  when 'N'
+		@direction = 'E'
+	  when 'E'
+		@direction = 'S'
+	  when 'S'
+		@direction = 'W'
+	  when 'W'
+		@direction = 'N'
+	  end
+	end
+
+	def foward 
+	  case @direction
+	  when 'N'
+		@y += 1
+	  when 'E'
+		@x += 1
+	  when 'S'
+		@y -= 1
+	  when 'W'
+		@x -= 1
+	  end
+	end
+
 end
