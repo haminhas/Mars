447aa6908734eb14d39fed4d1adc084a884d5bf1
Hassan Minhas <Hassan@Hassans-MacBook-Pro.local>
2016-02-26 19:38:19 +0000

    added method to check whether robot is on the board

--- Robot.rb
+++ Robot.rb
@@ -3,6 +3,8 @@ class Robot
   attr_accessor :y
   attr_accessor :direction
   attr_accessor :commands
+  attr_accessor :on_board
+  @on_board
   def initialize(x, y, direction, commands)
 	@x = x
 	@y = y
@@ -47,5 +49,13 @@ class Robot
   	when 'W'
 	  @x -= 1
     end
+  end
+
+  def check_on_board
+  	if $top_right[0] >= @x && @x >= 0 && $top_right[1] >= @y && @y >= 0
+  	  @on_board = true
+  	else
+  	  @on_board = false
+  	end
   end
 end
